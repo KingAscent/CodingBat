7c7979b17e48758436717b8cb9981578f31eaa16
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 14:43:46 -0700

    Update frontTimes.java
    Format the comment properly, left the problem in the initial comment at the top, and updated the solution.

--- frontTimes.java
+++ frontTimes.java
@@ -1,23 +1,18 @@
 /*
-Given a string and a non-negative int n, we'll say that the front of the 
-string is the first 3 chars, or whatever is there if the string is less 
-than length 3. Return n copies of the front;
+ * Given a string and a non-negative int n, we'll say that the front of the 
+ * string is the first 3 chars, or whatever is there if the string is less 
+ * than length 3. Return n copies of the front;
+ */
 
-frontTimes("Chocolate", 2) → "ChoCho"
-frontTimes("Chocolate", 3) → "ChoChoCho"
-frontTimes("Abc", 3) → "AbcAbcAbc"
-*/
-
-public String frontTimes(String str, int n){
-  String front = "";
-  String copies = "";
-  if(str.length() < 3){
-    front = str;
-  }else{
-    front = str.substring(0, 3);
-  }
-  for(int i = 0; i < n;i++){
-    copies += front;
-  }
-  return copies;
+public String frontTimes(String str, int n) {
+  String times = "";
+  int charCount = str.length();
+  // In the event the string is longer than 3 characters
+  if(3 < charCount)
+    charCount = 3;
+  
+  String front = str.substring(0, charCount);
+  for(int i = 0; i < n; i++)
+    times += front;
+  return times;
 }
