360270f2dd905f016ad418858ffb4ab4cb8b8a7c
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 15:31:58 -0700

    Update doubleX.java

--- doubleX.java
+++ doubleX.java
@@ -1,5 +1,5 @@
 /*
- *Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+ * Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
  */
 
 boolean doubleX(String str) {
