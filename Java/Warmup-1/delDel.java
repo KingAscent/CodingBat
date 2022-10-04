ce1e3f1090beca29eb8c487cf988dc5ea1c3b2d8
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 16:59:29 -0700

    Update delDel.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/delDel.java
+++ Java/Warmup-1/delDel.java
@@ -1,3 +1,8 @@
+/*
+ * Given a string, if the string "del" appears starting at index 1, return a string where
+ * that "del" has been deleted. Otherwise, return the string unchanged.
+ */
+
 public String delDel(String str) {
   if(4 <= str.length()){
     if(str.substring(1, 4).equals("del")){
