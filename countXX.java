07cfab2a7d76b50e8fbf8cc6a1c8f63a4034c24e
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 14:59:08 -0700

    Update countXX.java
    Format the comment properly, left the problem in the initial comment at the top, and updated the solution.

--- countXX.java
+++ countXX.java
@@ -1,17 +1,13 @@
 /*
-Count the number of "xx" in the given string. We'll say that overlapping 
-is allowed, so "xxx" contains 2 "xx".
+ * Count the number of "xx" in the given string. We'll say that overlapping 
+ * is allowed, so "xxx" contains 2 "xx".
+ */
 
-countXX("abcxx") → 1
-countXX("xxx") → 2
-countXX("xxxx") → 3
-*/
-
-int countXX(String str){
+int countXX(String str) {
   int count = 0;
-  for(int i = 0; i < str.length() - 1; i++)
-    if(str.substring(i, i + 2).equals("xx")){
-      count++;
+  for(int i = 0; i < str.length() - 1; i++){
+    if(str.substring(i, i + 2).equals("xx"))
+    count++;
   }
   return count;
 }
