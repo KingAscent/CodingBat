de9d0532e195aae5f35ea14868a18d766ead8fba
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 15:34:33 -0700

    Update stringSplosion.java
    Format the comment properly, left the problem in the initial comment at the top, and updated the solution.

--- stringSplosion.java
+++ stringSplosion.java
@@ -1,16 +1,11 @@
 /*
-Given a non-empty string like "Code" return a string like "CCoCodCode".
-
-
-stringSplosion("Code") → "CCoCodCode"
-stringSplosion("abc") → "aababc"
-stringSplosion("ab") → "aab"
-*/
-
-public String stringSplosion(String str){
+ * Given a non-empty string like "Code" return a string like "CCoCodCode".
+ */
+ 
+public String stringSplosion(String str) {
   String splosion = "";
-  for(int i = 0; i < str.length(); i++){
+  for(int i = 0; i <= str.length(); i++){
     splosion += str.substring(0, i);
   }
-  return splosion += str;
+  return splosion;
 }
