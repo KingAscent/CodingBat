ee15e0be8aa55c1765928182c518cef097521a57
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:08:18 -0700

    Update icyHot.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/icyHot.java
+++ Java/Warmup-1/icyHot.java
@@ -1,3 +1,7 @@
+/*
+ * Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+ */
+
 public boolean icyHot(int temp1, int temp2) {
   return (temp1 < 0 && temp2 > 100) || (100 < temp1 && temp2 < 0);
 }
