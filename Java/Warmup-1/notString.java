348058b60fcd2cc43b5263ae92921907b3e6a738
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:31:22 -0700

    Update notString.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/notString.java
+++ Java/Warmup-1/notString.java
@@ -1,3 +1,9 @@
+/*
+ * Given a string, return a new string where "not " has been added to the front. However, 
+ * if the string already begins with "not", return the string unchanged. 
+ * Note: use .equals() to compare 2 strings.
+ */
+
 public String notString(String str) {
   if(3 <= str.length() && str.substring(0, 3).equals("not")){
     return str;
