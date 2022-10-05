2e52bbcbae2a696f69712c7c305b1b20e5c6daf5
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:24:05 -0700

    Update mixStart.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/mixStart.java
+++ Java/Warmup-1/mixStart.java
@@ -1,3 +1,7 @@
+/*
+ * Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+ */
+
 public boolean mixStart(String str) {
   return (3 <= str.length() && str.substring(1, 3).equals("ix"));
 }
