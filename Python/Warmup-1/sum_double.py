0065a757f84edacc34dce6ba06fd4f49e7c3d44e
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:58:48 -0700

    Update sum_double.py
    Added the problem in the comment above the solution.

--- Python/Warmup-1/sum_double.py
+++ Python/Warmup-1/sum_double.py
@@ -1,3 +1,7 @@
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True
+# if we are on vacation. We sleep in if it is not a weekday or we're on vacation. 
+# Return True if we sleep in.
+
 def sum_double(a, b):
   if(a == b):
     return 2* (a + b)
