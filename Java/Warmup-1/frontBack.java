77f87c961a1afdde70949d271578c6957c832508
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:06:25 -0700

    Update frontBack.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/frontBack.java
+++ Java/Warmup-1/frontBack.java
@@ -1,3 +1,7 @@
+/*
+ * Given a string, return a new string where the first and last chars have been exchanged.
+ */
+
 public String frontBack(String str) {
   if(str.length() <= 1){
     return str;
