c5a9741750e5704f20535abc2dc7a0d9549e67fa
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:34:07 -0700

    Update posNeg.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/posNeg.java
+++ Java/Warmup-1/posNeg.java
@@ -1,3 +1,8 @@
+/*
+ * Given 2 int values, return true if one is negative and one is positive.
+ * Except if the parameter "negative" is true, then return true only if both are negative.
+ */
+
 public boolean posNeg(int a, int b, boolean negative) {
   if(negative){
     return (a < 0 && b < 0);
