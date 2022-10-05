64777ff2116a22b2aaeb482166fc2fce39fe2ecb
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:37:25 -0700

    Update startOz.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/startOz.java
+++ Java/Warmup-1/startOz.java
@@ -1,3 +1,8 @@
+/*
+ * Given a string, return a string made of the first 2 chars (if present), however include first char
+ * only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+ */
+
 public String startOz(String str) {
   String start = "";
   if(1 <= str.length() && str.charAt(0) == 'o'){
