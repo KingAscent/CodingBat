dcb472ba2550039b616f18df1de7bbfe56fd5911
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:10:16 -0700

    Update in3050.java
    Added the problem in the comment above the solution.

--- Java/Warmup-1/in3050.java
+++ Java/Warmup-1/in3050.java
@@ -1,3 +1,8 @@
+/*
+ * Given 2 int values, return true if they are both in the range 30..40
+ * inclusive, or they are both in the range 40..50 inclusive.
+ */
+
 public boolean in3050(int a, int b) {
   if(30 <= a && a <= 40 && 30 <= b && b <= 40)
     return true;
