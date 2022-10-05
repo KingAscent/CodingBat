92719bcb1618833a4309e7a4aacde74ebd74b0c9
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-04 17:40:42 -0700

    Update stringTimes.java
    Added the problem in the comment above the solution.

--- Java/Warmup-2/stringTimes.java
+++ Java/Warmup-2/stringTimes.java
@@ -1,3 +1,7 @@
+/*
+ * Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+ */
+
 public String stringTimes(String str, int n) {
   String times = "";
   for(int i = 0; i < n; i++){
