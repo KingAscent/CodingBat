f1545cde8cc966cf1bf4b96586a9914347d405f7
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-09 19:40:08 -0700

    Update cigar_party.py
    Cleaning the clause on return.

--- Python/Logic-1/cigar_party.py
+++ Python/Logic-1/cigar_party.py
@@ -5,4 +5,4 @@
 # values is successful, or False otherwise.
 
 def cigar_party(cigars, is_weekend):
-  return ((40 <= cigars) and (cigars <= 60)) or ((40 <= cigars) and is_weekend)
+  return ((40 <= cigars<= 60) or ((40 <= cigars) and is_weekend))
