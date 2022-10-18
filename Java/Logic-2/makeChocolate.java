63a8a18e51663ff3bb7ce60615685ef741216c2d
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-18 00:08:11 -0700

    Create makeChocolate.java

---
+++ Java/Logic-2/makeChocolate.java
@@ -0,0 +1,23 @@
+/*
+ * We want make a package of goal kilos of chocolate. We have small bars
+ * (1 kilo each) and big bars (5 kilos each). Return the number of small
+ * bars to use, assuming we always use big bars before small bars.
+ * Return -1 if it can't be done.
+ */
+
+public int makeChocolate(int small, int big, int goal) {
+  // Checks how many bigs we have
+  int bigsNeeded = goal / 5;
+  if(bigsNeeded <= big){
+    goal -= bigsNeeded * 5;
+  }else{
+    goal -= big * 5;
+  }
+  
+  // Checks if we have enough smalls to make up for any remainder
+  if(goal <= small)
+    return goal;
+  
+  // If this point is reached, then it is not possible to build the package
+  return -1;
+}
