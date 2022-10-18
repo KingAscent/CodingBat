daa84c4f2bc1a1d482ff72bbbae1de74f1621a86
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-18 10:27:51 -0700

    Update loneSum.java
    Shorten the code and add comments for clarity.

--- Java/Logic-2/loneSum.java
+++ Java/Logic-2/loneSum.java
@@ -4,15 +4,21 @@
  */
 
 public int loneSum(int a, int b, int c) {
+  // Check if a, b, or c equal one another
   if(a == b || a == c || b == c){
+    // If all three numbers are the same, return 0
     if(a == b && a == c && b == c)
       return 0;
-    if(a == b && a != c && b != c)
+      
+      // If only two numbers match, return the one that does not match
+    if(a == b)
       return c;
-    if(b == c && a != b && a != b)
+    if(b == c)
       return a;
-    if(a == c && a != b && b != c)
+    if(a == c)
       return b;
   }
+  
+  // All three numbers are unique, return their sum
   return a + b + c;
 }
