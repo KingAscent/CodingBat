84e0090115cd0595c4996b48d603b310f9a9a5d7
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-18 10:29:32 -0700

    Update make_bricks.py
    Added the question into the top comment.

--- Python/Logic-2/make_bricks.py
+++ Python/Logic-2/make_bricks.py
@@ -1,3 +1,9 @@
+# We want to make a row of bricks that is goal inches long. We have a number of
+# small bricks (1 inch each) and big bricks (5 inches each). Return True if it
+# is possible to make the goal by choosing from the given bricks. This is a
+# little harder than it looks and can be done without any loops.
+# See also: Introduction to MakeBricks
+
 def make_bricks(small, big, goal):
   # Ensure we have enough brikcs to meet our goal
   if small + (big * 5) < goal:
