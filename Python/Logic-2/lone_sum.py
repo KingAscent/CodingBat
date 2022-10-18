ee9c97835d028096c9cc5d590245cfb518f7fb03
Vin <86906764+KingAscent@users.noreply.github.com>
2022-10-18 10:35:18 -0700

    Update lone_sum.py

--- Python/Logic-2/lone_sum.py
+++ Python/Logic-2/lone_sum.py
@@ -1,10 +1,12 @@
+# Given 3 int values, a b c, return their sum. However, if one of the values
+# is the same as another of the values, it does not count towards the sum.
 def lone_sum(a, b, c):
   # Check if a, b, or c equal one another
   if (a == b) or (a == c) or (b == c):
     # If all three numbers are the same, return 0
     if (a == b) and (a == c) and (b == c):
       return 0
-    
+      
     # If only two numbers match, return the one that does not match
     if (a == b):
       return c
